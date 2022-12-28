6fc36222327db763cd52247d070cea862e22d35c
XaverWeste <westxave@kkg-do.eu>
2022-12-28 13:05:14 +0100

    fightscenecontroller

--- Assets/Scripts/FightSceneController.cs
+++ Assets/Scripts/FightSceneController.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class FightSceneController : MonoBehaviour
 {
+    
+    
     // Start is called before the first frame update
     void Start()
     {
@@ -12,6 +14,16 @@ public class FightSceneController : MonoBehaviour
 
     // Update is called once per frame
     void Update()
+    {
+        
+    }
+
+    public void SetUpFight()
+    {
+        
+    }
+
+    public void ReadInput(string s)
     {
         
     }
