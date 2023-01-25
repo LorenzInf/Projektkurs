eccbe6885bb681b695a0a4c47ce486bd0ba5bfca
Xaver Maximilian <westxave@kkg-do.eu>
2023-01-25 10:27:38 +0100

    Room ist jetzt eine Klasse und Bug fix

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -30,43 +30,67 @@ public class PlayerController : MonoBehaviour{
     public void HandleMovement(){
 		float x=0.0f,y=0.0f;
         if (Input.GetKey(KeyCode.S))
-            y-=2;
+            y-=3;
         if (Input.GetKey(KeyCode.A))
-        	x-=2;
+        	x-=3;
         if (Input.GetKey(KeyCode.W))
-            y+=2;
+            y+=3;
         if (Input.GetKey(KeyCode.D))
-            x+=2;
+            x+=3;
 		if(left==x>0&&x!=0.0f){
 			gameObject.transform.Rotate(0.0f, 180.0f, 0.0f, Space.Self);
 			left=!left;
 		}
 		gameObject.transform.position += new Vector3(x,y,0.0f)*Time.deltaTime;
 		MapGen.Dir dir=MovedToDoor();
-		if(dir!=MapGen.Dir.Null&&level!=null)
+		if (dir != MapGen.Dir.Null)
 			level.Move(dir);
+		ValidatePosition();
     }
 
 	private MapGen.Dir MovedToDoor(){
 		Vector3 v=gameObject.transform.position;
+		if (level == null) return MapGen.Dir.Null;
 		if(v.y<2.5&&v.y>-2.5&&v.x>-9.3&&v.x<9.3) return MapGen.Dir.Null;
 		if((v.y>2.5)&&v.x<1&&v.x>-1){
-			gameObject.transform.position = new Vector3(v.x,-2.51f,0.0f)*Time.deltaTime;
-			return MapGen.Dir.Up;
-		}
-		if((v.y<-2.5)&&v.x<1&&v.x>-1){
-			gameObject.transform.position = new Vector3(v.x,2.49f,0.0f)*Time.deltaTime;
-			return MapGen.Dir.Down;
-		}
-		if((v.x>9.3)&&v.y<1&&v.y>-1){
-			gameObject.transform.position = new Vector3(9.29f,v.y,0.0f)*Time.deltaTime;
-			return MapGen.Dir.Right;
-		}
-		if((v.x<-9.3)&&v.y<1&&v.y>-1){
-			gameObject.transform.position = new Vector3(-9.31f,v.y,0.0f)*Time.deltaTime;
-			return MapGen.Dir.Left;
+			if (level.CanMove(MapGen.Dir.Up)){
+				gameObject.transform.position = new Vector3(v.x,-2.49f,v.z)*Time.deltaTime;
+				return MapGen.Dir.Up;
+			}
+		}else if((v.y<-2.5)&&v.x<1&&v.x>-1){
+			if (level.CanMove(MapGen.Dir.Down)){
+				gameObject.transform.position = new Vector3(v.x, 2.49f, v.z) * Time.deltaTime;
+				return MapGen.Dir.Down;
+			}
+		}else if((v.x>9.3)&&v.y<1&&v.y>-1){
+			if (level.CanMove(MapGen.Dir.Right)){
+				gameObject.transform.position = new Vector3(9.29f, v.y, v.z) * Time.deltaTime;
+				return MapGen.Dir.Right;
+			}
+		}else if((v.x<-9.3)&&v.y<1&&v.y>-1){
+			if (level.CanMove(MapGen.Dir.Left)){
+				gameObject.transform.position = new Vector3(-9.29f, v.y, v.z) * Time.deltaTime;
+				return MapGen.Dir.Left;
+			}
 		}
 		return MapGen.Dir.Null;
+	}
+
+	private void ValidatePosition()
+	{
+		Vector3 v=gameObject.transform.position;
+		if (v.x<-9.4){
+			gameObject.transform.position = new Vector3(-9.39f,v.y,v.z);
+		}
+		if (v.x>9.4){
+			gameObject.transform.position = new Vector3(9.39f,v.y,v.z);
+		}
+		if (v.y<-2.6){
+			gameObject.transform.position = new Vector3(v.x,-2.59f,v.z);
+		}
+		if (v.y>2.6){
+			gameObject.transform.position = new Vector3(v.x,2.59f,v.z);
+		}
 	}
 
     public void TakeDamage(int amount) {
